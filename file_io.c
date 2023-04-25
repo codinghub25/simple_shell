d8351220f2a3663e36c9500f43ee3348e29bc606
Operaconga <operahub39@gmail.com>
2023-04-25 04:01:28 -0700

    my_get_history_file - gets the history file

--- file_io.c
+++ file_io.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * my_ get_history_file - gets the history file
+ * my_get_history_file - gets the history file
  * @info: parameter struct
  *
  * Return: allocated string containg history file
